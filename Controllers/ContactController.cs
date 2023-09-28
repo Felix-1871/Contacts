440e16e63fb0c5e089bb1c223810e36ed14c2e46
Felix-1871 <99283735+Felix-1871@users.noreply.github.com>
2023-09-29 00:44:30 +0200

    Add project files.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+public class Contact
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Pole 'Imię' jest wymagane.")]
+    [MaxLength(50)]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Pole 'Nazwisko' jest wymagane.")]
+    [MaxLength(50)]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Pole 'Email' jest wymagane.")]
+    [MaxLength(100)]
+    [DataType(DataType.EmailAddress)]
+    [Index(IsUnique = true)] // Unikalny email
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Pole 'Hasło' jest wymagane.")]
+    [MinLength(8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "Hasło powinno spełniać podstawowe standardy złożoności.")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Pole 'Kategoria' jest wymagane.")]
+    public ContactCategory Category { get; set; }
+
+    public ContactSubCategory? SubCategory { get; set; }
+
+    [Required(ErrorMessage = "Pole 'Telefon' jest wymagane.")]
+    [MaxLength(15)]
+    [DataType(DataType.PhoneNumber)]
+    public string PhoneNumber { get; set; }
+
+    [Required(ErrorMessage = "Pole 'Data urodzenia' jest wymagane.")]
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+}
+
+public enum ContactCategory
+{
+    Służbowy,
+    Prywatny,
+    Inny
+}
+
+public enum ContactSubCategory
+{
+    Szef,
+    Klient,
+    Inna
+}
