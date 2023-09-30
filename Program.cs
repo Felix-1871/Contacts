9cd68f0fdc9be16ea6548f86a9c88911b56468ad
Felix-1871 <99283735+Felix-1871@users.noreply.github.com>
2023-09-30 23:31:43 +0200

    Allowed vue to connect to api

--- Program.cs
+++ Program.cs
@@ -11,6 +11,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ContactContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ContactDB")));
 
+// Configure server to allow CORS from Vue app (localhost:3000) to get and post data
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,5 +37,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors("CorsPolicy");
 
 app.Run();
